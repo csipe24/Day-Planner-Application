88b93e393cba9ba82bef4121003d5ab788aeee52
Chris Sipe <csipe24@gmail.com>
2020-05-02 09:29:14 -0700

    Few changes, working on last items

--- script.js
+++ script.js
@@ -3,52 +3,47 @@ var dateEl = $("#date")
 $(dateEl).text(moment().format("dddd, MMMM Do"));
 
 // Save Tasks for each hour with local storage 
-
 // renderTasks();
 
-var save = $(".save");
+var save = $(".save"),
+    tasks = JSON.parse(localStorage.getItem("tasks")) || {};
+    console.log(tasks);
 
-  
+// Loop Over Each Save Button to input object items
+save.each(function(){
+    var buttonHour = $(this).data("hour"),
+        taskInput = $("#task-" + buttonHour);  
+    taskInput.val( tasks ["#task" + buttonHour])
+    })
+
+// Onclick Event for save buttons
 $(save).on( 'click', function() {
-    event.preventDefault();
     console.log("Saved!");
 
 var buttonHour = $(this).data("hour");
     console.log(buttonHour);
 
-var taskInput = $("#task-" + buttonHour);
+var taskInput = $("#task-" + buttonHour),
+    taskDes = taskInput.val();
     console.log(taskInput.val());
 
+    tasks["#task" + buttonHour] = taskDes;
 
-localStorage.setItem("taskInput", )
-
+localStorage.setItem("tasks",JSON.stringify(tasks));
 });
 
-// Define Render Tasks
+
+// // Define Render Tasks
 // function renderTasks (){
-// var 
-
-// initials = localStorage.getItem("initials"),\
-
-// if (task === null) {
-//     return;
-//   }
-
-//   initialsSpan.textContent = task
 // };
     
-
-
-// var highScoreFormEl =$("form"),
-//     scoreEl =$("#score");
-//     submit =$("#submit");
-//     initialsInput =$("#initialsInput")
 
 
 // Getting Current Hour of Day
 // * Determine if each hour block is past, preset or future
 var hourBlocks = $("#hour"),
     currentHour = moment().hour(Number);
+    console.log(currentHour);
 
 //     -Getting current hour moment().hour()
 
